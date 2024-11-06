f2012119a72e9be81034f8bbed37ed14347fb820
itsPixel <brandonllanes16@gmail.com>
2024-11-05 18:04:43 -0800

    fix table schema and initialize some values

--- inventory-service/src/main/resources/db/migration/V1__init.sql
+++ inventory-service/src/main/resources/db/migration/V1__init.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `t_inventory`
 (
     `id` bigInt(20) NOT NULL AUTO_INCREMENT,
-    `sku_code` varchar(255),
-    `quantity` int(11),
+    `sku_code` varchar(255) NOT NULL,
+    `quantity` int(11) NOT NULL,
     PRIMARY KEY (`id`)
 );
