53202278552641d7eecaeb7bd9e4d97b08aa0c1d
itsPixel <brandonllanes16@gmail.com>
2024-11-06 16:43:06 -0800

    add basic inventory tests

--- inventory-service/src/test/java/com/techie/microservices/inventory/InventoryServiceApplicationTests.java
+++ inventory-service/src/test/java/com/techie/microservices/inventory/InventoryServiceApplicationTests.java
@@ -1,15 +1,45 @@
 package com.techie.microservices.inventory;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.utility.DockerImageName;
+import static org.hamcrest.Matchers.equalTo;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class InventoryServiceApplicationTests {
 
+	@ServiceConnection
+	static MySQLContainer<?> mySQLContainer = new MySQLContainer<>(DockerImageName.parse("mysql:8.3.0"));
+
+	@LocalServerPort
+	private Integer port;
+
+	@BeforeEach
+	void setup() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
+
+	static {
+		mySQLContainer.start();
+	}
+
 	@Test
-	void contextLoads() {
+	void checkIfIphone15InStock() {
+		RestAssured.given()
+				.queryParam("skuCode", "iphone_15")
+				.queryParam("quantity", 10)
+				.get("/api/inventory")
+				.then()
+				.statusCode(200)
+				.body(equalTo("true"));
 	}
 
 }
