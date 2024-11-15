4b11111abaa8ce6cdf2ada70b6a3c246b18fefaa
itsPixel <brandonllanes16@gmail.com>
2024-11-15 14:55:03 -0800

    make get products public

--- api-gateway/src/main/java/com/programming/techie/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/programming/techie/gateway/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.programming.techie.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,8 +16,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(freeResourceUrls)
-                        .permitAll()
+                        .requestMatchers(freeResourceUrls).permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/product").permitAll()
                         .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
